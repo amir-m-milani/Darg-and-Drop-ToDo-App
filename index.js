fad15818f38e7b73d02b9e9fa0a95b473be84701
amir.m.milani <amirmilani08@gmail.com>
2024-02-04 16:43:41 +0330

    make function add job to the category = Today

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const addJobButton = document.querySelector(".add-button");
+const addButton = document.querySelector(".add-button");
 const closeModal = document.querySelector(".modal_ .main .close");
+const insertJobButton = document.querySelector(".submit-button");
 
-addJobButton.addEventListener("click", OpenModal);
+addButton.addEventListener("click", OpenModal);
 closeModal.addEventListener("click", CloseModal);
+insertJobButton.addEventListener("click", AddJob);
 
 function OpenModal() {
     document.querySelector(".overlay").classList.remove("d-none");
@@ -13,4 +15,21 @@ function CloseModal() {
     document.querySelector(".overlay").classList.add("d-none");
     document.querySelector(".modal_ .main").classList.remove("open");
 
+}
+
+function AddJob() {
+    const jobTitle = document.querySelector(".modal_ input[name=job]").value;// get the job title from the input
+    const category = document.querySelector("#Today .jobs");// select the category (default = Today)
+    let job = document.createElement("div"); // create element
+    // add the job title
+    job.innerHTML = `
+    <div class="job">
+        <h4>${jobTitle}</h4>
+        <div class="check-box">
+        </div>
+    </div>`;
+    category.appendChild(job);// add the job to category
+    // close the modal
+    CloseModal();
+    // console.log(jobTitle);
 }
