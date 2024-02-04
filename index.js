045561481494a1998b8bca761041ac63abd9c7eb
amir.m.milani <amirmilani08@gmail.com>
2024-02-04 17:04:20 +0330

    add dragging function to all jobs

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const addButton = document.querySelector(".add-button");
 const closeModal = document.querySelector(".modal_ .main .close");
 const insertJobButton = document.querySelector(".submit-button");
+const jobs = document.querySelectorAll(".job");
+const jobsConatainers = document.querySelectorAll(".jobs");
 
 addButton.addEventListener("click", OpenModal);
 closeModal.addEventListener("click", CloseModal);
 insertJobButton.addEventListener("click", AddJob);
+
+//drag every job
+jobs.forEach(job => {
+    job.addEventListener("dragstart", () => {
+        job.classList.add("dragging");
+    });
+    job.addEventListener("dragend", () => {
+        job.classList.remove("dragging");
+    });
+});
+
+// add dragging job to category
+jobsConatainers.forEach(jobContainer => {
+    jobContainer.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const darggingJob = document.querySelector(".dragging");
+        jobContainer.appendChild(darggingJob);
+    });
+});
 
 function OpenModal() {
     document.querySelector(".overlay").classList.remove("d-none");
@@ -23,11 +44,17 @@ function AddJob() {
     let job = document.createElement("div"); // create element
     // add the job title
     job.innerHTML = `
-    <div class="job">
+    <div class="job" draggable="true">
         <h4>${jobTitle}</h4>
         <div class="check-box">
         </div>
     </div>`;
+    job.addEventListener("dragstart", () => {
+        job.classList.add("dragging");
+    });
+    job.addEventListener("dragend", () => {
+        job.classList.remove("dragging");
+    });
     category.appendChild(job);// add the job to category
     // close the modal
     CloseModal();
